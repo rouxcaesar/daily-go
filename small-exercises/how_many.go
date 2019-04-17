3ee06c5fafccf597937df98a5e52eed1bdd9bcfa
rouxcaesar <jrzerwick@gmail.com>
2019-04-17 13:32:05 -0400

    Started how_many.go exercise

---
+++ small-exercises/how_many.go
@@ -0,0 +1,24 @@
+// Write a function that counts the number of occurrences of each element in a given array
+
+// Expected Input:
+// vehicles = [ 'car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck' ]
+
+// Expected Output:
+// car => 4
+// truck => 3
+// SUV => 1
+// motorcycle => 2
+
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func howMany([]string) [string]int {
+
+}
+
+
