419e8b0a37fc4bf0b7d6375ab0d63783121ab3af
rouxcaesar <jrzerwick@gmail.com>
2019-04-18 11:26:19 -0400

    Started reverse_it_pt1.go exercise

---
+++ small-exercises/reverse_it_pt1.go
@@ -0,0 +1,37 @@
+// Write a method that takes one argument, a string, and returns a new string with the words in reverse order.
+// 
+// Examples:
+// 
+// puts reverse_sentence('') == ''
+// puts reverse_sentence('Hello World') == 'World Hello'
+// puts reverse_sentence('Reverse these words') == 'words these Reverse'
+
+// Input: string
+// Output: new string with words in reverse order
+
+// DS: slice to represent space-delimited words
+
+// Approach:
+// - Take the input string and split by whitespace into words. Store in a slice.
+// - Create a new string variable to store reversed words.
+// - Iterate through slice of words and concatenate each word to the string variable with a trailing whitespace (except for the last word)
+
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+
+}
+
+func splitString(sentence string) []string {
+
+}
+
+func joinWords(words []string) string {
+
+}
+
